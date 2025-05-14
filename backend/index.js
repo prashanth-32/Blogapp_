42961b6d1d7530c2913cb6bf3e48c5d7000d769a
Sai Prashanth <saiprashanth381@gmail.com>
2025-05-14 09:25:00 +0530

    cors change

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://blogapp-frontend-0fpl.onrender.com',
     credentials:true
 }))
 app.use(cookieParser());
